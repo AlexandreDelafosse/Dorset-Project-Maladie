56db0089c572189fed4f709afbc2bdc8328d09dd
Hugo Perrain <hugo.perrain@edu.devinci.fr>
2021-12-12 18:25:30 +0100

    add html

--- LegalMention.php
+++ LegalMention.php
@@ -9,6 +9,30 @@
     <script src="/script/script.js"></script>
 </head>
 <body>
-    
+<?php include "repetition/header.php" ?>
+
+<h1>Private Policy</h1>
+
+<section class="section">
+    <div>When you make a charitable donation through Schizophrenia Society of Canada’s (SSC) website, you are entrusting us with very important financial and personal information, and we understand that you may have concerns about privacy issues on the Internet. SSC feels that protecting the privacy of our donors is one of our most important jobs.</div>
+    <div>Your personal information, although it is shared with Schizophrenia Society of Canada, belongs to you. When we refer to “personal information”, we mean any information about an identifiable individual, whether it concerns your email address, where you live, your interests or which organizations you support. </div>
+    <div>We feel that it is your right to be able to give in a safe and secure environment, and to know how your personal information is being used. </div>
+    <div>Your personal information will never be shared, traded, sold or used in any manner not explicitly approved by you. </div>
+</section>
+
+<section class="section">
+    <div>We do not swap mailing lists, share donor information or sell (or even collect) phone numbers. Only Sschizophrenia Society of Canada staff are allowed to access this personal information. Our schizophrenia association is required to store your personal information by the Canada Revenue Agency (CRA). It also helps protect you in the case of a lost or missing receipt, because we can reissue a receipt for your tax purposes. </div>
+    <div>Finally, the storage of personal information helps protect you against credit card fraud, because the records of all transactions are kept for reference in the case of a dispute. During the course of your charitable donation to our schizophrenia association, your credit card information is processed by Moneris. The only other circumstances under which we would divulge any personal donor information to a third party is upon subpoena or order from a legitimate court, police agency, the CRA, or if requested by a credit card issuer in the case of a dispute. </div>
+    <div>In any case when your information is shared, SSC will demand reasonable assurance that third parties will protect your information in conformity with our privacy practices.</div>
+</section>
+
+<section class="section">   
+    <div>While sending your personal information to Schizophrenia Society of Canada is necessary for us to issue you a tax receipt, you have the right to choose not to share that information with the charity to whom you make the charitable donation. By choosing ‘Anonymous’ or any other contact preference on the charitable donation form, your personal information will not be viewable by the charity you donated to. </div>
+    <div>The only information accessible will be charitable donation date, time, amount, and the type of credit card used. From time to time, SSC may share statistics that describe how our visitors use our schizophrenia association’s site. This might include information like the average donation amount, how donations break down by geographic area and what times of the day have the heaviest donation traffic. None of these statistics will ever reveal any personally-identifiable or individual information. </div>
+    <div>This information allows Schizophrenia Society of Canada to better serve donors and use its marketing resources more efficiently. Privacy is important to you, and it is important to us.</div>
+</section>    
+
+
+<?php include "repetition/footer.php" ?>
 </body>
 </html>
