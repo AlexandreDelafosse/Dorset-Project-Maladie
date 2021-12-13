b2702d77e640af29a2bd33370d9573ce9bb38978
Cameron-ux <70761331+Cameron-ux@users.noreply.github.com>
2021-12-13 23:26:33 +0000

    homepage done

--- Homepage.php
+++ Homepage.php
@@ -41,13 +41,80 @@
         <div class="global2">
             <div class="text2">
                 <img src="img/noun_Mental Health_2686181.png" alt="head">
-                <p class="scs">SCS aims to support and accompany people affected by schizophrenia. And more generally, SCS is
+                <p class="scs">SCS aims to support and accompany people affected by schizophrenia. And more generally,
+                    SCS is
                     involved in communicating about the disease in order to educate about the reality of schizophrenia.
                 </p>
             </div>
         </div>
 
     </div>
+
+    <div class="first">
+        <div class="orange2">
+            <h1 class="pre">PRESENTATION OF THE DISEASE</h1>
+        </div>
+        <img class="ques" src="img/_.png" alt="?">
+
+        <p class="sch">Schizophrenia is a <span class="violet2">brain disease</span> that affects thinking, <span
+                class="violet2">feelings</span> and <span class="violet2">emotions</span>, as well as <span
+                class="violet2">perceptions</span> and
+            <span class="violet2">behaviour</span>. However, not all of these functions are affected at the same time
+            and to the same degree.
+            Many people with schizophrenia can behave perfectly normally for long periods of time.
+        </p>
+        <p class="sch2">Schizophrenia often causes a number of <span class="violet2">cognitive deficits</span> that
+            affect attention, memory, learning and
+            information processing.</p>
+
+        <img class="eyes" src="img/noun_creative doodle eyes_2208292.png" alt="eyes">
+        <img class="head" src="img/noun_Schizophrenia_1000160.png" alt="sch">
+        <img class="sleep" src="img/noun_Insomnia_2751357.png" alt="sleep">
+    </div>
+
+    <div class="first">
+        <div class="orange3">
+            <h1 class="pre">ABOUT US</h1>
+        </div>
+
+        <p class="text3">
+            <span class="violet2">The Schizophrenia Society of Canada</span> (SSC) was started as a non-profit,
+            charitable organization in Toronto in
+            1979 by Bill Jefferies and his wife who had a son living with schizophrenia.
+            Within a few years there were provincial schizophrenia societies across Canada. As parents and caregivers
+            they were quite aware of the <span class="violet2">stigma</span>, <span class="violet2">prejudice</span>,
+            and <span class="violet2">discrimination</span> associated with schizophrenia, one of the
+            world’s most devastating <span class="violet2">mental illnesses</span>.
+        </p>
+        <p class="text4">Their lived experience as family members fueled their passion and vision that more could be
+            done, not only
+            those living with schizophrenia, but also for families. Families had no voice. Thus, the SSC’s mission had a
+            strong emphasis on <span class="violet2">advocacy</span>, which remains to this day.</p>
+
+        <img class="logo2" src="img/logo_05.png" alt="logo">
+
+        <p class="text4">Today, SSC has a strong conviction that things can definitely be different and better for those experiencing
+            early psychosis and for the one in one hundred who will develop schizophrenia.</p>
+        <p class="text4">SSC is committed to transforming how people think, with knowledge and youthful enthusiasm. We seek to be an
+            inspirational visionary in advocating for a <span class="violet2">transformed mental health system</span> that is based upon the recovery
+            philosophy.</p>
+
+    </div>
+
+    <div class="first">
+        <div class="orange4">
+            <h1 class="sim">SIMULATION</h1>
+        </div>
+    </div>
+
+    <div class="simu">
+            <div class="start">
+                <h2>The Schizophrenia</h2>
+                <p class="last">simulation</p>
+
+                <a href="Simulation.php"><button>Start</button></a>
+            </div>
+        </div>
 
 
     <?php include "repetition/footer.php" ?>
