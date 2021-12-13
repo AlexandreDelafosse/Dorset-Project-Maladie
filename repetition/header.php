27236acbe76c11af0583a0e511c39df0b3d1886f
Delafosse Alexandre <alex.delaf@outlook.fr>
2021-12-13 18:11:05 +0000

    fix header

--- repetition/header.php
+++ repetition/header.php
@@ -1,6 +1,7 @@
 <header>
     <ul id="menu">
-        <img class="logo" src="img\logo_05.png" alt="logo">
+        
+        <li><a href="Homepage.php"> <img class="logo" src="img\logo_05.png" alt="logo"  width="120" height="70"></li></a>
         <li><a href="AssoPres.php">The Foundation</a></li>
         <li><a href="DiseaseExplanation.php">Our fight</a></li>
         <li><a href="Simulation.php">Experience</a></li>
