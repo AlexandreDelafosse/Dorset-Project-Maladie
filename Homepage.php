da3b383677645987f994fad9632c4c7405916a53
Cameron-ux <70761331+Cameron-ux@users.noreply.github.com>
2021-12-13 20:22:02 +0000

    first part homepage

--- Homepage.php
+++ Homepage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,12 +11,46 @@
     <link rel="stylesheet" href="css/homepage.css">
     <script src="/script/script.js"></script>
 </head>
+
 <body>
-<?php include "repetition/header.php" ?>
+    <?php include "repetition/header.php" ?>
 
-        <img class="affiche" src="img/affiche04.jpg" alt="affiche">
+    <img class="affiche" src="img/affiche04.jpg" alt="affiche">
+
+    <div class="first">
+        <div class="orange">
+            <h1 class="home">HOME</h1>
+        </div>
+
+        <div class="global1">
+
+            <div class="text1">
+                <p class="violet">“Build a Canada where people living with
+                    early psychosis and schizophrenia achieve their potential.”
+                </p>
+                <p class="black">Described 100 years ago, schizophrenia remains one of the most mysterious diseases in
+                    psychiatry, as
+                    well as one of the most severe.
+                </p>
+            </div>
+
+            <img class="bird" src="img/bird.png" alt="bird">
+
+        </div>
+
+        <div class="global2">
+            <div class="text2">
+                <img src="img/noun_Mental Health_2686181.png" alt="head">
+                <p class="scs">SCS aims to support and accompany people affected by schizophrenia. And more generally, SCS is
+                    involved in communicating about the disease in order to educate about the reality of schizophrenia.
+                </p>
+            </div>
+        </div>
+
+    </div>
 
 
-<?php include "repetition/footer.php" ?>
+    <?php include "repetition/footer.php" ?>
 </body>
+
 </html>
