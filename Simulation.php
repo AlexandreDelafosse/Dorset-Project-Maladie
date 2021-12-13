40b052db15ea18de2b3ff54c27f7c128e8c078e1
Delafosse Alexandre <alex.delaf@outlook.fr>
2021-12-13 16:43:05 +0000

    style.css add

--- Simulation.php
+++ Simulation.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simulation</title>
     <link rel="stylesheet" href="css/Simulation.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="/script/script.js"></script>
 </head>
 
