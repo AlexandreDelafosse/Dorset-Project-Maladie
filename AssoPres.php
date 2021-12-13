fcaf20a90fb45d14683e04f785e9cc3c7bf45fb8
Delafosse Alexandre <alex.delaf@outlook.fr>
2021-12-13 16:30:19 +0000

    ajout repetitions

--- AssoPres.php
+++ AssoPres.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +9,14 @@
     <link rel="stylesheet" href="css/AssoPres.css">
     <script src="/script/script.js"></script>
 </head>
+
 <body>
-    
+    <?php include "repetition/header.php" ?>
+
+
+
+    <?php include "repetition/footer.php" ?>
+
 </body>
+
 </html>
