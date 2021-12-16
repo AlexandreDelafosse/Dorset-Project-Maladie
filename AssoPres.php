87269f2a2c41e6f7eea3a7e2b8ff5855496aacb8
Delafosse Alexandre <alex.delaf@outlook.fr>
2021-12-16 22:49:22 +0000

    page finit

--- AssoPres.php
+++ AssoPres.php
@@ -26,28 +26,59 @@
             <article id="article1">
 
                 <img id="logopart1" src="img\ssc-logo-RGB_bilingual-stacked-small 1.png" alt="logoasso">
-            
+
 
             </article>
 
             <article id="article2">
 
-                <p> The Schizophrenia Society of Canada (SSC) was started as a non-profit, charitable organization in Toronto in
-                    1979 by Bill Jefferies and his wife who had a son living with schizophrenia. Within a few years there were
+                <p> The Schizophrenia Society of Canada (SSC) was started as a non-profit, charitable organization in
+                    Toronto in
+                    1979 by Bill Jefferies and his wife who had a son living with schizophrenia. Within a few years
+                    there were
                     provincial schizophrenia societies across Canada.
                     <br>
                     <br>
-                    As parents and caregivers they were quite aware of the stigma, prejudice, and discrimination associated with
+                    As parents and caregivers they were quite aware of the stigma, prejudice, and discrimination
+                    associated with
                     schizophrenia, one of the world’s most devastating mental illnesses. </p>
 
             </article>
 
 
-    </section>
+        </section>
 
     </div>
 
     <img id="image1" src="img\MaskGroup.png" alt="banderole">
+
+    <hr id="hr1">
+
+    <h1> OUR VALUES </h1>
+
+    <hr id="hr2">
+
+    <p id="textebas"> The Schizophrenia Society of Canada’s <b>mission</b>, <b>goals</b>, and <b>objectives</b> are informed by the following core values :
+            <br>
+            <br>
+        <div class="textetcarre"> <div class="text"></div> <p>Schizophrenia and early psychosis are medical illnesses that, like other medical illnesses, have variable
+        expression/effects on symptoms, function and response to treatments.</p> </div>
+
+        <div class="textetcarre"> <div class="text"></div><p> and early psychosis are caused by a number of different factors; from multiple genetic or
+        environmental factors or from a combination of both.</p></div>
+
+        <div class="textetcarre"> <div class="text"> </div><p>The SSC fully supports the important role of research in all areas related to schizophrenia and early psychosis
+        (biological, psychological, spiritual, and social determinants of health)</p></div>
+
+        <div class="textetcarre"> <div class="text"></div><p> Persons with schizophrenia and early psychosis are entitled to efficient multi-disciplinary and integrated
+        evidence-informed treatment and community support services.</p></div>
+
+        <div class="textetcarre"> <div class="text"> </div><p>Persons at the early phases of their illness are entitled to real secondary prevention (early intervention and
+        treatment) through specialized first episode early psychosis clinics and their collaborators.</p></div>
+        
+    </p>
+
+    <a id="buttondonate">  DONATE</a>
 
     <?php include "repetition/footer.php" ?>
 
